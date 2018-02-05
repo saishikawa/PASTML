30157ac9d528832cdf8484a41e8b5bcbf33cadcf
Sohta Ishikawa <s.ishikawa.biol.phylo@gmail.com>
2018-02-05 20:19:47 +0900

    Add files via upload

--- asrml.h
+++ asrml.h
@@ -27,7 +27,7 @@
 
 #define MAXLNAME 255
 #define MAXNSP 50000
-#define MAXPOLY 50000
+#define MAXPOLY 10000
 #define MAXCHAR 50
 #define MIN_BRLEN    1e-5
 #define MAX_TREELENGTH    10000000 /* more or less 10MB for a tree file in NH format */
